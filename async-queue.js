dff6b155141f15011eec7b1c59dee89bac9ee941
Josh Froelich <joshua.froelich@gmail.com>
2020-01-25 19:58:15 -0800

    remove comments

--- async-queue.js
+++ async-queue.js
@@ -1,11 +1,3 @@
-/**
- * @typedef ListNode
- * @property {ListNode} next
- */
-
-/**
- * @class AsyncQueue
- */
 class AsyncQueue {
   constructor() {
     this.head = undefined;
@@ -18,23 +10,8 @@ class AsyncQueue {
     this.busyDelay = 0;
   }
 
-  /**
-   * Enqueue a function call. This immediately places the call in
-   * the queue and immediately returns a promise that will
-   * eventually resolve once the call completes.
-   *
-   * @template T the type of the return value of func
-   *
-   * @param {function} func A function to call
-   * @param  {...any} args Parameters to function when it is called
-   * @returns {Promise<T>} Settles when the function call completes
-   * and either resolves to the function's return value or rejects
-   * with an error.
-   */
   run(func, ...args) {
-    /** @type {Task<T>} */
     const task = new Task(func, ...args);
-
     listAppend(this, task);
 
     if (!this.paused && !this.isSaturated()) {
@@ -45,10 +22,7 @@ class AsyncQueue {
     return task.promise;
   }
 
-  /** Whether the queue is running at max concurrency */
   isSaturated() {
-    console.assert(this.runningTaskCount <= this.concurrency,
-      'concurrency limit exceeded');
     return this.runningTaskCount >= this.concurrency;
   }
 
@@ -57,7 +31,6 @@ class AsyncQueue {
     clearTimeout(this.timer);
   }
 
-  /** @param {boolean} [immediately] whether to run in this tick or the next */
   resume(immediately = true) {
     this.paused = false;
     if (immediately) {
@@ -72,10 +45,6 @@ class AsyncQueue {
   }
 }
 
-/**
- * Count the number of nodes in a list
- * @param {{ head: ListNode, tail: ListNode }} list
- */
 function listLength(list) {
   let length = 0;
   let node = list.head;
@@ -86,12 +55,6 @@ function listLength(list) {
   return length;
 }
 
-/**
-
- *
- * @param {{ head: ListNode, tail: ListNode }} list
- * @param {{ next: ListNode }} node
- */
 function listAppend(list, node) {
   if (list.tail) {
     list.tail.next = node;
@@ -102,11 +65,6 @@ function listAppend(list, node) {
   list.tail = node;
 }
 
-/**
- * Remove the first node from the list and return it
- * Returns undefined if the list is empty
- * @param {object} list
- */
 function listPop(list) {
   const node = list.head;
   if (node) {
@@ -117,38 +75,14 @@ function listPop(list) {
   return node;
 }
 
-/**
- * @param {AsyncQueue} queue
- * @param {number} delay ms
- */
 function reschedule(queue, delay) {
   if (!queue.paused) {
     queue.timer = setTimeout(drain, delay, queue);
   }
 }
 
-/**
- * @param {AsyncQueue} queue
- * @returns {Promise<void>}
- */
 async function drain(queue) {
-  // If we are draining right now, we assume responsibility for
-  // rescheduling ourselves, and want to cancel any kind of
-  // other enqueued request to drain. This is harmless if that
-  // is not the case.
   clearTimeout(queue.timer);
-
-  // There are 3 main reasons to not drain, only one of which
-  // involves rescheduling ourself to run again:
-  // 1) we were instructed to do nothing (paused), and
-  //    we should not reschedule because we will explicitly
-  //    be told later (via resume) to try again
-  // 2) we are too busy right now, we should reschedule
-  //    because we can safely conclude trying again is worthwhile
-  // 3) there is nothing to do (and so we should also not
-  //    reschedule because schedule is done on add to avoid empty
-  //    poll)
-
   if (queue.paused) {
     return;
   }
@@ -179,24 +113,14 @@ async function drain(queue) {
   }
 }
 
-/**
- * @template T type of value from promise resolve
- */
 class Task {
-  /**
-   * @param {function} [func]
-   * @param {...any} args
-   */
   constructor(func, ...args) {
     this.func = func;
     this.args = args;
-
-    /** @type {Promise<T>} */
     this.promise = new Promise((resolve, reject) => {
       this.resolve = resolve;
       this.reject = reject;
     });
-
     this.next = undefined;
   }
 }
